cf10c301b39b22ea16e99fc2df401c2687cac8a0
Quentin WIDLOCHER <quentin.widlocher@viacesi.fr>
2019-03-22 17:11:37 +0100

    HammerJS, material icons et theme angular material

--- src/app/services/indexed-db.service.ts
+++ src/app/services/indexed-db.service.ts
@@ -5,19 +5,19 @@ import { Test } from '../classes/test';
 @Injectable({
   providedIn: 'root'
 })
+// On fait une classe qui étend Dexie et qui va nou permettre de gérer l'IDB
 export class IndexedDbService extends Dexie {
 
+  // Les tables sont publiques
   tests: Dexie.Table<Test, number>;
 
   constructor() {
     super('test');
 
-    this.init();
-  }
-
-  init() {
+    // On déclare la structure des tables
     this.version(1).stores({ tests: '++id, test' });
 
+    // On lie les structures aux objets
     this.tests = this.table('tests');
   }
 }
