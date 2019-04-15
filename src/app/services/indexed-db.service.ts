0df9a85655f330f679a6ffd7d9e47a2b1830e88c
Psilonnix <psilonnix@gmail.com>
2019-04-15 14:30:47 +0200

    Menu projets

--- src/app/services/indexed-db.service.ts
+++ src/app/services/indexed-db.service.ts
@@ -1,6 +1,5 @@
 import { Injectable } from '@angular/core';
 import Dexie from 'dexie';
-import { Test } from '../classes/test';
 
 @Injectable({
   providedIn: 'root'
@@ -8,16 +7,10 @@ import { Test } from '../classes/test';
 // On fait une classe qui étend Dexie et qui va nou permettre de gérer l'IDB
 export class IndexedDbService extends Dexie {
 
-  // Les tables sont publiques
-  tests: Dexie.Table<Test, number>;
+
 
   constructor() {
     super('test');
 
-    // On déclare la structure des tables
-    this.version(1).stores({ tests: '++id, test' });
-
-    // On lie les structures aux objets
-    this.tests = this.table('tests');
   }
 }
