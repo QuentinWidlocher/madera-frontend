ef698ac2be1f50938375922fb6f5012112767edc
Quentin <quentin.widlocher@viacesi.fr>
2019-04-18 09:18:42 +0200

    Fix netlify build

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,8 +2,6 @@ import { Component } from '@angular/core';
 import { IndexedDbService } from './services/indexed-db.service';
 import 'hammerjs/hammer.min';
 import { ConnectivityService } from './services/connectivity.service';
-import { DevisSwService } from './services/service-workers/DEVIS-sw.service';
-import { Devis } from './classes/devis';
 import { ProjetSwService } from './services/service-workers/projet-sw.service';
 import { Projet } from './classes/projet';
 import { ClientSwService } from './services/service-workers/client-sw.service';
