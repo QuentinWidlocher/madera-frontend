44c3865a2042f6fa2a4398fbf2ea4b5ab6cb9320
Quentin <quentin.widlocher@viacesi.fr>
2019-04-23 18:44:29 +0200

    Corrige quelques erreurs pour Netlify

--- src/app/components/page/create-users/create-users.component.ts
+++ src/app/components/page/create-users/create-users.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit, EventEmitter, Output } from '@angular/core';
+import { Utilisateur } from 'src/app/classes/utilisateur';
 
 @Component({
   selector: 'app-create-users',
@@ -7,14 +8,35 @@ import { Component, OnInit, EventEmitter, Output } from '@angular/core';
 })
 export class CreateUsersComponent implements OnInit {
 
-  events: string[] = [];
-  opened: boolean;
+  utilisateurs: Utilisateur[] = [];
+  currentUtilisateur: Utilisateur;
+
+  utilisateurListLoading: boolean = true;
+  utilisateurListIndex: number;
+
+  passwordHide: boolean;
 
   @Output() onHamburger: EventEmitter<void> = new EventEmitter<void>();
 
   constructor() { }
 
   ngOnInit() {
+  }
+
+  selectUtilisateur(utilisateur: Utilisateur, index: number) {
+    // if (this.utilisateurForm && this.utilisateurForm.dirty) {
+    //   return;
+    // }
+
+    this.currentUtilisateur = utilisateur;
+    this.utilisateurListIndex = index;
+
+    // this.refreshForm();
+  }
+
+  createUtilisateur() {
+    this.currentUtilisateur = Utilisateur.newEmpty();
+    // this.changeEditMode(true, true);
   }
 
 }
