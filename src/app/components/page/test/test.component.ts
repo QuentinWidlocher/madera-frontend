5acb9917f4e1a8fddb8300605308704b4ba4eb70
Quentin <quentin.widlocher@viacesi.fr>
2019-04-24 18:27:29 +0200

    Vide la page de test et l'enlève de la navbar
    C'était plus très utile et on pourra toujours la réutiliser en utilisant l'url

--- src/app/components/page/test/test.component.ts
+++ src/app/components/page/test/test.component.ts
@@ -1,9 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import { Caracteristique } from 'src/app/classes/caracteristique';
-import { CaracteristiqueSwService } from 'src/app/services/service-workers/caracteristique-sw.service';
-import { ConnectivityService } from 'src/app/services/connectivity.service';
-import { Client } from 'src/app/classes/client';
-import { ClientSwService } from 'src/app/services/service-workers/client-sw.service';
 
 @Component({
   selector: 'app-test',
@@ -12,25 +7,11 @@ import { ClientSwService } from 'src/app/services/service-workers/client-sw.serv
 })
 export class TestComponent implements OnInit {
 
-  listClient: Client[];
-  currentClient: Client = Client.newEmpty();
 
-  constructor(private clientSw: ClientSwService) {
+  constructor() {
   }
   
   ngOnInit() {
-    this.clientSw.getAll().then(clients => {
-      this.listClient = clients;
-    }).then(() => {
-      console.log(this.listClient);
-    });
-  }
-
-  addClient() {
-    this.clientSw.add(this.currentClient).then(() => {
-      this.listClient.push(this.currentClient);
-      this.currentClient = Client.newEmpty();
-    });
   }
 
 }
